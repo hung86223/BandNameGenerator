1d2f155697c67bdc23f208435751571f81bf06ce
ben.huang52 <ben.huang@titansoft.com.sg>
2019-09-26 11:05:35 +0800

    create project

---
+++ BandNameGeneratorTest/BandNameGeneratorTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BandNameGeneratorTest
+{
+    [TestClass]
+    public class BandNameGeneratorTest
+    {
+        [TestMethod]
+        public void GiveNounNamePutTheInFront()
+        {
+            var bandNameGenerator = new BandNameGenerator();
+            var name = "Dolphin";
+
+            var bandName = bandNameGenerator.GenerateBandName(name);
+
+            Assert.AreEqual("The " + name, bandName);
+        }
+    }
+
+    public class BandNameGenerator
+    {
+        public string GenerateBandName(string giveName)
+        {
+            return "The " + giveName;
+        }
+    }
+}
---
+++ BandNameGeneratorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BandNameGeneratorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BandNameGeneratorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("98978ca8-2302-4ca2-ba71-57c173fd7473")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
